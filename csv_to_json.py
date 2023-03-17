cddb40f7255b5856401b8f6a9e7a60463c4c5442
giokim12 <109334986+giokim12@users.noreply.github.com>
2023-03-17 13:51:12 +0900

    yahoo query

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,9 +2,9 @@ import csv
 import json
 
 data = []
-with open('all_stocks_basic_data.csv', 'r') as csv_file:
+with open('aapl.csv', 'r') as csv_file:
     reader = csv.DictReader(csv_file)
     data = list(reader)
 
-with open('all_stocks_basic_data.json', 'w') as json_file:
+with open('aapl.json', 'w') as json_file:
     json.dump(data, json_file)
